df0ffd475f37ebb3e40bc013fab3333e818034cf
mustafasahin0 <sahinmustafaenes4@gmail.com>
2026-01-08 23:08:45 -0700

    Remove sitemap integration from Astro configuration and update package.json and package-lock.json accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://mustafasahin.dpdns.org',
   output: 'server',
   adapter: node({ mode: 'standalone' }),
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), tailwind()],
   image: {
     service: { entrypoint: 'astro/assets/services/noop' }
   },
